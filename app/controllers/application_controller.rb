5d01b270ca90a397485f316a78015eb273ed556d
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-08-09 12:18:06 -0300

    responding with json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+
+  def respond_to_on_destroy
+    head :no_content
+  end
 end
