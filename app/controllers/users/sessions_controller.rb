200fe66e90be5bd4ef964c48b702a1a792828c97
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-08-12 20:00:04 -0300

    basic devise jwt setup

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    super do |resource|
+      token = Warden::JWTAuth::UserEncoder.new.call(resource, :user, nil).first
+      return render json: { user: resource.as_json, token: token }, status: :ok
+    end
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
